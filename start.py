05cc21e384a0f558662b54debd8f8285aaf1a2c2
zasp4you <72430918+zasp4you@users.noreply.github.com>
2021-08-20 19:18:38 +0530

    Delete start.py

--- start.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-  await message.reply("Hey there, I'm AutoChannelCaption Bot for Zasp4yoy. Zakir Ayoub Developed me on 20 August 2021. \n\nSource code => https://github.com/zasp4you/AutoCaptionBot")
