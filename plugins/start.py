607389e19b09d603b8157b7202c79f1d002c9a3b
zasp4you <72430918+zasp4you@users.noreply.github.com>
2021-08-20 19:50:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,2 +1,7 @@
+
+
 from pyrogram import Client, filters
-@Client.on_message(filters.command("start") & filters.private) async def start(client, message): await message.reply("Hey there, I'm AutoChannelCaption Bot for Zasp4you. Zakir Ayoub Developed me on 20 August 2021. \n\nSource code => https://github.com/zasp4you/AutoCaptionBot")
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+  await message.reply("Hey there, I'm channel caption bot. \n\nSource code => https://github.com/samadii/ChannelAutoCaption")
